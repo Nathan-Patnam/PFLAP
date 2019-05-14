52b6bc051b11e055564eb6432ff7e8bcfc7a67dc
Nathan Patnam <patnamnv@email.wofford.edu>
2019-05-13 21:28:25 -0400

    refractore nav bar buttons into own component class

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
 '''
-Canvas stress
-=============
-
-This example tests the performance of our Graphics engine by drawing large
-numbers of small squares. You should see a black canvas with buttons and a
-label at the bottom. Pressing the buttons adds small colored squares to the
-canvas.
-
 '''
 
 from kivy.uix.button import Button
@@ -18,61 +10,85 @@ from kivy.graphics import Color, Rectangle
 from random import random as r
 from functools import partial
 
-
 class DfaScreen(App):
-
-    def add_rects(self, label, wid, count, *largs):
-        label.text = str(int(label.text) + count)
-        with wid.canvas:
-            for x in range(count):
-                Color(r(), 1, 1, mode='hsv')
-                Rectangle(pos=(r() * wid.width + wid.x,
-                               r() * wid.height + wid.y), size=(20, 20))
-
-    def double_rects(self, label, wid, *largs):
-        count = int(label.text)
-        self.add_rects(label, wid, count, *largs)
-
-    def reset_rects(self, label, wid, *largs):
-        label.text = '0'
-        wid.canvas.clear()
-
     def build(self):
-        self.wid = Widget()
-        self.create_bottom_nav_bars()
+        self.current_mode = "MOVE STATE"
+
         root = BoxLayout(orientation='vertical')
-        root.add_widget(self.wid)
-        root.add_widget(self.layout)
+        canvas = Widget()
+        root.add_widget(canvas)
+
+        canvas_nav_bar = NavBar(canvas)
+        canvas.bind(on_touch_down=self.onPressed)
+        root.add_widget(canvas_nav_bar.layout)
         return root
     
-    def create_bottom_nav_bars(self):
+
+    def onPressed(self, a, event):
+        if self.current_mode == "ADD_STATE":
+            print("add state")
+        elif self.current_mode == "MOVE_STATE":
+            print("move state")
+        elif self.current_mode == "ADD_TRANSITION_ARROW":
+            print("add transition state")
+        elif self.current_mode == "DELETE_STATE":
+            print("delete state")
+        elif self.current_mode == "EDIT_STATE":
+            print("edit state")
+
+    
+class NavBar():
+    def __init__(self, wid):
+        self.widget = wid
         self.layout = BoxLayout(size_hint=(1, None), height=50)
         self.draw_move_state_button()
         self.draw_add_state_button()
         self.draw_add_transition_button()
         self.draw_edit_state_button()
         self.draw_delete_state_button()
+
     
+
     def draw_move_state_button(self):
-        self.add_button("Move state", self.add_rects)
-    
+        self.add_button("Move state", self.add_state)
+
     def draw_add_state_button(self):
-        self.add_button("Add state", self.add_rects)
+        self.add_button("Add state", self.change_to_add_state_mode)
+
+    def change_to_add_state_mode(self):
+        self.current_mode = "ADD STATE"
+
+
+
+    def add_state(self, label, wid, count, *largs):
+        with wid.canvas:
+            clicked_coordinates = wid.pos
+            x = clicked_coordinates[0]
+            y = clicked_coordinates[0]
+            RADIUS = 25
 
     def draw_add_transition_button(self):
         self.add_button("Add Transition Arrow", self.add_rects)
-    
+
     def draw_edit_state_button(self):
         self.add_button("Edit State", self.add_rects)
-    
+
     def draw_delete_state_button(self):
         self.add_button("Delete State", self.add_rects)
-    
+
     def add_button(self, name, on_click_callback):
         button_label = Label(text='0')
         button = Button(text=name,
-                        on_press=partial(on_click_callback, button_label, self.wid, 100))
+                        on_press=partial(on_click_callback, button_label, self.widget, 100))
         self.layout.add_widget(button)
+    
+    def add_rects(self, label, wid, count, *largs):
+        label.text = str(int(label.text) + count)
+        with wid.canvas:
+            for x in range(count):
+                Color(r(), 1, 1, mode='hsv')
+                Rectangle(pos=(r() * wid.width + wid.x,
+                                r() * wid.height + wid.y), size=(20, 20))
 
         
 if __name__ == '__main__':
