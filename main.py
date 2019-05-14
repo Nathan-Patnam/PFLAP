b310110547352bdc7f7d89ee3490acb85b2145ac
Nathan Patnam <patnamnv@email.wofford.edu>
2019-05-13 21:45:29 -0400

    made NavBar class take responsibility of keep track of the last button that was clicked

--- main.py
+++ main.py
@@ -12,33 +12,46 @@ from functools import partial
 
 class DfaScreen(App):
     def build(self):
-        self.current_mode = "MOVE STATE"
-
         root = BoxLayout(orientation='vertical')
         canvas = Widget()
         root.add_widget(canvas)
 
-        canvas_nav_bar = NavBar(canvas)
+        self.__canvas_nav_bar = NavBar(canvas)
         canvas.bind(on_touch_down=self.onPressed)
-        root.add_widget(canvas_nav_bar.layout)
+        
+        root.add_widget(self.__canvas_nav_bar.layout)
         return root
     
 
     def onPressed(self, a, event):
-        if self.current_mode == "ADD_STATE":
+        current_mode = self.get_current_mode()
+        print(current_mode)
+        if current_mode == "ADD_STATE":
             print("add state")
-        elif self.current_mode == "MOVE_STATE":
+        elif current_mode == "MOVE_STATE":
             print("move state")
-        elif self.current_mode == "ADD_TRANSITION_ARROW":
+        elif current_mode == "ADD_TRANSITION_ARROW":
             print("add transition state")
-        elif self.current_mode == "DELETE_STATE":
+        elif current_mode == "DELETE_STATE":
             print("delete state")
-        elif self.current_mode == "EDIT_STATE":
+        elif current_mode == "EDIT_STATE":
             print("edit state")
-
+        
+        """
+        with wid.canvas:
+            clicked_coordinates = wid.pos
+            x = clicked_coordinates[0]
+            y = clicked_coordinates[0]
+            RADIUS = 25
+        """
+    
+    def get_current_mode(self):
+        current_mode = self.__canvas_nav_bar.get_current_mode()
+        return current_mode
     
 class NavBar():
     def __init__(self, wid):
+        self.current_mode = "MOVE_STATE"
         self.widget = wid
         self.layout = BoxLayout(size_hint=(1, None), height=50)
         self.draw_move_state_button()
@@ -50,32 +63,35 @@ class NavBar():
     
 
     def draw_move_state_button(self):
-        self.add_button("Move state", self.add_state)
+        self.add_button("Move state", self.change_state_to_move_state)
+    
+    def change_state_to_move_state(self, label, wid, count, *largs):
+        self.current_mode = "MOVE_STATE"
 
     def draw_add_state_button(self):
-        self.add_button("Add state", self.change_to_add_state_mode)
+        self.add_button("Add state", self.change_state_to_add_state)
 
-    def change_to_add_state_mode(self):
-        self.current_mode = "ADD STATE"
-
-
-
-    def add_state(self, label, wid, count, *largs):
-        with wid.canvas:
-            clicked_coordinates = wid.pos
-            x = clicked_coordinates[0]
-            y = clicked_coordinates[0]
-            RADIUS = 25
+    def change_state_to_add_state(self, label, wid, count, *largs):
+        self.current_mode = "ADD_STATE"
 
     def draw_add_transition_button(self):
-        self.add_button("Add Transition Arrow", self.add_rects)
+        self.add_button("Add Transition Arrow", self.change_state_to_add_transition)
+
+    def change_state_to_add_transition(self, label, wid, count, *largs):
+        self.current_mode = "ADD_TRANSITION_ARROW"
 
     def draw_edit_state_button(self):
-        self.add_button("Edit State", self.add_rects)
+        self.add_button("Edit State", self.change_state_to_edit_state)
+    
+    def change_state_to_edit_state(self, label, wid, count, *largs):
+        self.current_mode = "EDIT_STATE"
 
     def draw_delete_state_button(self):
-        self.add_button("Delete State", self.add_rects)
+        self.add_button("Delete State", self.change_state_to_delete_state)
 
+    def change_state_to_delete_state(self, label, wid, count, *largs):
+        self.current_mode = "DELETE_STATE"
+    
     def add_button(self, name, on_click_callback):
         button_label = Label(text='0')
         button = Button(text=name,
@@ -89,6 +105,9 @@ class NavBar():
                 Color(r(), 1, 1, mode='hsv')
                 Rectangle(pos=(r() * wid.width + wid.x,
                                 r() * wid.height + wid.y), size=(20, 20))
+    
+    def get_current_mode(self):
+        return self.current_mode
 
         
 if __name__ == '__main__':
